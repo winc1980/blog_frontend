a97688e6d78d14e80d6ec2e064ea43920826c7b6
empelt <tetsuyakato2001@yahoo.co.jp>
2022-12-06 23:31:02 +0900

    envの設定

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import Providers from 'next-auth/providers'
 import GithubProvider from 'next-auth/providers/github'
 
 export default NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.NEXT_PUBLIC_GITHUB_ID ?? '',
